802ef76a6146557193e29080dff8fe45f5e7f583
vinicius <vinicius6.lopes6@gmail.com>
2024-09-07 18:19:21 -0300

    Criação do projeto: Back end do app runmail.

---
+++ src/main/java/com/runmail/runmail/RunmailApplication.java
@@ -0,0 +1,13 @@
+package com.runmail.runmail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RunmailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RunmailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/runmail/runmail/RunmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.runmail.runmail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
